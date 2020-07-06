1c02ea8d25dc261f5226abad2105c0cf3d5a2b3a
dori <dori.claudino@gmail.com>
2020-07-05 20:16:43 -0400

    channels and userInfo

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
 import { Grid } from './styles';
-
 import ServerList from '../ServerList';
 import ServerName from '../ServerName';
 import ChannelInfo from '../ChannelInfo';
+import ChannelList from '../ChannelList';
+import UserInfo from '../UserInfo';
 
 const Layout: React.FC = () => {
   return ( 
@@ -12,6 +12,8 @@ const Layout: React.FC = () => {
       <ServerName />
       <ServerList />
       <ChannelInfo />
+      <ChannelList />    
+      <UserInfo />  
     </Grid>
   )
 };
